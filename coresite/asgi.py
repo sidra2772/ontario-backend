516288a77dd55a10f750a2e6d448a102edcabad3
Shamasulhaq Python Developer <ulhaqshams924@gmail.com>
2025-03-13 00:47:59 +0500

    add field in service model discounted_price

--- coresite/asgi.py
+++ coresite/asgi.py
@@ -5,23 +5,23 @@ ASGI config for coresite project.
 import os
 import django
 from django.core.asgi import get_asgi_application
-
-# Initialize Django FIRST
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coresite.settings')
-django.setup()
-
-# Now import other dependencies AFTER initialization
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middlewares import TokenAuthMiddleware
-import chat.routing as channels_routing
+import chat.routing as chat_routing
 import notification.routing as notification_routing
 
+# Set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coresite.settings')
+
+# Initialize Django (Important for Channels)
+django.setup()
+
+# Define ASGI application
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": TokenAuthMiddleware(
         URLRouter(
-            channels_routing.websocket_urlpatterns +
-            notification_routing.websocket_urlpatterns
+            chat_routing.websocket_urlpatterns + notification_routing.websocket_urlpatterns
         )
-    )
+    ),
 })
